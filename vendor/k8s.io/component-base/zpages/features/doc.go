1a0c3960cdc700e6faf8f81ea755b4654d28ecc5
Rinaldy-Adin <rinaldyadin@gmail.com>
2025-04-19 16:01:37 +0700

    feat: module versions

--- vendor/k8s.io/component-base/zpages/features/doc.go
+++ vendor/k8s.io/component-base/zpages/features/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2024 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,13 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package
-// +k8s:protobuf-gen=package
-// +k8s:conversion-gen=k8s.io/apiextensions-apiserver/pkg/apis/apiextensions
-// +k8s:defaulter-gen=TypeMeta
-// +k8s:openapi-gen=true
-// +k8s:prerelease-lifecycle-gen=true
-// +groupName=apiextensions.k8s.io
-
-// Package v1 is the v1 version of the API.
-package v1 // import "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+// Package features contains a separate feature set specifically designed for
+// managing zpages related features. These feature gates control the
+// availability and behavior of various zpages within Kubernetes components.
+// New zpages added to Kubernetes components should utilize this feature set
+// to ensure proper management of their availability.
+package features
