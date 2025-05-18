c3dd5a429bbf354b020e2c1cde9ef7db30ee4d3d
Rinaldy-Adin <rinaldyadin@gmail.com>
2025-05-19 06:54:40 +0700

    refactor: general functions to different file

--- pkg/framework/plugins/nodeusagevariation/loadvariationriskbalancing.go
+++ pkg/framework/plugins/nodeusagevariation/loadvariationriskbalancing.go
@@ -8,19 +8,14 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/descheduler/pkg/api"
 	"sigs.k8s.io/descheduler/pkg/descheduler/evictions"
 	podutil "sigs.k8s.io/descheduler/pkg/descheduler/pod"
-	"sigs.k8s.io/descheduler/pkg/framework/plugins/nodeutilization/normalizer"
 	frameworktypes "sigs.k8s.io/descheduler/pkg/framework/types"
-	"sigs.k8s.io/descheduler/pkg/utils"
 )
 
 const LoadVariationRiskBalancingPluginName = "LoadVariationRiskBalancing"
-
-type continueEvictionCond func(NodeDistributionInfo, resource.Quantity) bool
 
 var _ frameworktypes.BalancePlugin = &LoadVariationRiskBalancing{}
 
@@ -31,28 +26,6 @@ type LoadVariationRiskBalancing struct {
 	podFilter         func(pod *v1.Pod) bool
 	avgUsageClient    usageClient
 	stdDevUsageClient usageClient
-}
-
-type ResourceUsageDistributions struct {
-	avg    api.Percentage
-	stdDev api.Percentage
-}
-
-type NodeDistributionUsage struct {
-	node     *v1.Node
-	avg      resource.Quantity
-	stdDev   resource.Quantity
-	capacity resource.Quantity
-	allPods  []*v1.Pod
-}
-
-// NodeInfo is an entity we use to gather information about a given node. here
-// we have its resource usage as well as the amount of available resources.
-// we use this struct to carry information around and to make it easier to
-// process.
-type NodeDistributionInfo struct {
-	NodeDistributionUsage
-	available resource.Quantity
 }
 
 func NewLoadVariationRiskBalancing(
@@ -189,12 +162,12 @@ func (l *LoadVariationRiskBalancing) Balance(ctx context.Context, nodes []*v1.No
 		return nil
 	}
 
-	sortNodesByUsageRisk(highRiskNodes, false)
+	l.sortNodesByUsageRisk(highRiskNodes, false)
 
 	// this is a stop condition for the eviction process. we stop as soon
 	// as the node usage drops below the threshold.
 	continueEvictionCond := func(nodeInfo NodeDistributionInfo, totalAvailableUsage resource.Quantity) bool {
-		if !isNodeAboveTargetRisk(nodeInfo) {
+		if !l.isNodeAboveTargetRisk(nodeInfo) {
 			return false
 		}
 
@@ -210,6 +183,30 @@ func (l *LoadVariationRiskBalancing) Balance(ctx context.Context, nodes []*v1.No
 		nodeLimit = l.args.EvictionLimits.Node
 	}
 
+	sortPodsByRisk := func(
+		node NodeDistributionInfo,
+		pods []*v1.Pod,
+		avgUsageClient, stdDevUsageClient usageClient,
+	) {
+		podAvgUsage := make(map[string]*resource.Quantity)
+		podStdDevUsage := make(map[string]*resource.Quantity)
+
+		for _, pod := range pods {
+			avg, _ := avgUsageClient.podUsage(pod)
+			stdDev, _ := stdDevUsageClient.podUsage(pod)
+
+			podAvgUsage[pod.Name] = avg[MetricResource]
+			podStdDevUsage[pod.Name] = stdDev[MetricResource]
+		}
+
+		sort.Slice(pods, func(i, j int) bool {
+			pi := podAvgUsage[pods[i].Name].MilliValue() + podStdDevUsage[pods[i].Name].MilliValue()
+			pj := podAvgUsage[pods[j].Name].MilliValue() + podStdDevUsage[pods[j].Name].MilliValue()
+
+			return pi < pj
+		})
+	}
+
 	evictPodsFromSourceNodes(
 		ctx,
 		l.args.EvictableNamespaces,
@@ -222,265 +219,13 @@ func (l *LoadVariationRiskBalancing) Balance(ctx context.Context, nodes []*v1.No
 		continueEvictionCond,
 		l.avgUsageClient,
 		l.stdDevUsageClient,
+		sortPodsByRisk,
 		nodeLimit,
 	)
 
 	return nil
 }
 
-func evictPodsFromSourceNodes(
-	ctx context.Context,
-	evictableNamespaces *api.Namespaces,
-	sourceNodes, destinationNodes []NodeDistributionInfo,
-	podEvictor frameworktypes.Evictor,
-	evictOptions evictions.EvictOptions,
-	podFilter func(pod *v1.Pod) bool,
-	resourceNames []v1.ResourceName,
-	continueEviction continueEvictionCond,
-	avgUsageClient, stdDevUsageClient usageClient,
-	maxNoOfPodsToEvictPerNode *uint,
-) {
-	available := assessAvailableResourceInNodes(destinationNodes)
-	klog.V(1).InfoS("Total capacity to be moved", usageToKeysAndValues(available)...)
-
-	destinationTaints := make(map[string][]v1.Taint, len(destinationNodes))
-	for _, node := range destinationNodes {
-		destinationTaints[node.node.Name] = node.node.Spec.Taints
-	}
-
-	for _, node := range sourceNodes {
-		klog.V(3).InfoS(
-			"Evicting pods from node",
-			"node", klog.KObj(node.node),
-			"avg", node.avg,
-		)
-
-		nonRemovablePods, removablePods := classifyPods(node.allPods, podFilter)
-		klog.V(2).InfoS(
-			"Pods on node",
-			"node", klog.KObj(node.node),
-			"allPods", len(node.allPods),
-			"nonRemovablePods", len(nonRemovablePods),
-			"removablePods", len(removablePods),
-		)
-
-		if len(removablePods) == 0 {
-			klog.V(1).InfoS(
-				"No removable pods on node, try next node",
-				"node", klog.KObj(node.node),
-			)
-			continue
-		}
-
-		klog.V(1).InfoS(
-			"Evicting pods based on risk, if they have same priority, they'll be evicted based on QoS tiers",
-		)
-
-		sortPodsByRisk(removablePods, avgUsageClient, stdDevUsageClient)
-
-		if err := evictPods(
-			ctx,
-			evictableNamespaces,
-			removablePods,
-			node,
-			available,
-			destinationTaints,
-			podEvictor,
-			evictOptions,
-			continueEviction,
-			avgUsageClient,
-			maxNoOfPodsToEvictPerNode,
-		); err != nil {
-			switch err.(type) {
-			case *evictions.EvictionTotalLimitError:
-				return
-			default:
-			}
-		}
-	}
-}
-
-func evictPods(
-	ctx context.Context,
-	evictableNamespaces *api.Namespaces,
-	inputPods []*v1.Pod,
-	nodeInfo NodeDistributionInfo,
-	totalAvailableUsage resource.Quantity,
-	destinationTaints map[string][]v1.Taint,
-	podEvictor frameworktypes.Evictor,
-	evictOptions evictions.EvictOptions,
-	continueEviction continueEvictionCond,
-	usageClient usageClient,
-	maxNoOfPodsToEvictPerNode *uint,
-) error {
-	// preemptive check to see if we should continue evicting pods.
-	if !continueEviction(nodeInfo, totalAvailableUsage) {
-		return nil
-	}
-
-	// some namespaces can be excluded from the eviction process.
-	var excludedNamespaces sets.Set[string]
-	if evictableNamespaces != nil {
-		excludedNamespaces = sets.New(evictableNamespaces.Exclude...)
-	}
-
-	var evictionCounter uint = 0
-	for _, pod := range inputPods {
-		if maxNoOfPodsToEvictPerNode != nil && evictionCounter >= *maxNoOfPodsToEvictPerNode {
-			klog.V(3).InfoS(
-				"Max number of evictions per node per plugin reached",
-				"limit", *maxNoOfPodsToEvictPerNode,
-			)
-			break
-		}
-
-		if !utils.PodToleratesTaints(pod, destinationTaints) {
-			klog.V(3).InfoS(
-				"Skipping eviction for pod, doesn't tolerate node taint",
-				"pod", klog.KObj(pod),
-			)
-			continue
-		}
-
-		// verify if we can evict the pod based on the pod evictor
-		// filter and on the excluded namespaces.
-		preEvictionFilterWithOptions, err := podutil.
-			NewOptions().
-			WithFilter(podEvictor.PreEvictionFilter).
-			WithoutNamespaces(excludedNamespaces).
-			BuildFilterFunc()
-		if err != nil {
-			klog.ErrorS(err, "could not build preEvictionFilter with namespace exclusion")
-			continue
-		}
-
-		if !preEvictionFilterWithOptions(pod) {
-			continue
-		}
-
-		// in case podUsage does not support resource counting (e.g.
-		// provided metric does not quantify pod resource utilization).
-		unconstrainedResourceEviction := false
-		podUsageResourceList, err := usageClient.podUsage(pod)
-		if err != nil {
-			if _, ok := err.(*notSupportedError); !ok {
-				klog.Errorf(
-					"unable to get pod usage for %v/%v: %v",
-					pod.Namespace, pod.Name, err,
-				)
-				continue
-			}
-			unconstrainedResourceEviction = true
-		}
-
-		podUsage := podUsageResourceList[MetricResource]
-
-		if err := podEvictor.Evict(ctx, pod, evictOptions); err != nil {
-			switch err.(type) {
-			case *evictions.EvictionNodeLimitError, *evictions.EvictionTotalLimitError:
-				return err
-			default:
-				klog.Errorf("eviction failed: %v", err)
-				continue
-			}
-		}
-
-		if maxNoOfPodsToEvictPerNode == nil && unconstrainedResourceEviction {
-			klog.V(3).InfoS("Currently, only a single pod eviction is allowed")
-			break
-		}
-
-		evictionCounter++
-		klog.V(3).InfoS("Evicted pods", "pod", klog.KObj(pod))
-		if unconstrainedResourceEviction {
-			continue
-		}
-
-		subtractPodUsageFromNodeAvailability(&totalAvailableUsage, &nodeInfo, podUsage)
-
-		keysAndValues := []any{"node", nodeInfo.node.Name}
-		keysAndValues = append(keysAndValues, usageToKeysAndValues(nodeInfo.avg)...)
-		klog.V(3).InfoS("Updated node usage", keysAndValues...)
-
-		// make sure we should continue evicting pods.
-		if !continueEviction(nodeInfo, totalAvailableUsage) {
-			break
-		}
-	}
-	return nil
-}
-
-func subtractPodUsageFromNodeAvailability(
-	available *resource.Quantity,
-	nodeInfo *NodeDistributionInfo,
-	podUsage *resource.Quantity,
-) {
-	nodeInfo.avg.Sub(*podUsage)
-	available.Sub(*podUsage)
-}
-
-func assessAvailableResourceInNodes(
-	nodes []NodeDistributionInfo,
-) resource.Quantity {
-	available := resource.NewQuantity(0, resource.BinarySI)
-	for _, node := range nodes {
-		usage := node.avg
-
-		available.Add(node.available)
-		available.Sub(usage)
-	}
-
-	return *available
-}
-
-func rawUsageToPctUsageMap(
-	rawAvgUsage, rawStdDevUsage, rawCapacities map[string]resource.Quantity,
-) map[string]ResourceUsageDistributions {
-	avgUsage := normalizer.Normalize(
-		rawAvgUsage, rawCapacities, ResourceQuantityToPercentage,
-	)
-
-	stdDevUsage := normalizer.Normalize(
-		rawStdDevUsage, rawCapacities, ResourceQuantityToPercentage,
-	)
-
-	usageMap := make(map[string]ResourceUsageDistributions)
-	for nodeName := range rawAvgUsage {
-		usageMap[nodeName] = ResourceUsageDistributions{
-			avg:    avgUsage[nodeName],
-			stdDev: stdDevUsage[nodeName],
-		}
-	}
-
-	return usageMap
-}
-
-func getNodeUsageDistributionSnapshot(
-	nodes []*v1.Node,
-	avgUsageClient usageClient,
-	stdDevUsageClient usageClient,
-) (
-	map[string]*v1.Node,
-	map[string]resource.Quantity,
-	map[string]resource.Quantity,
-	map[string][]*v1.Pod,
-) {
-	rawAvgUsage := make(map[string]resource.Quantity)
-	rawStdDevUsage := make(map[string]resource.Quantity)
-	podListMap := make(map[string][]*v1.Pod)
-	nodesMap := make(map[string]*v1.Node)
-
-	for _, node := range nodes {
-		nodesMap[node.Name] = node
-		rawAvgUsage[node.Name] = *avgUsageClient.nodeUtilization(node.Name)[MetricResource]
-		rawStdDevUsage[node.Name] = *stdDevUsageClient.nodeUtilization(node.Name)[MetricResource]
-		podListMap[node.Name] = avgUsageClient.pods(node.Name)
-	}
-
-	return nodesMap, rawAvgUsage, rawStdDevUsage, podListMap
-}
-
-// TODO: implement
 func (l *LoadVariationRiskBalancing) classifyLoadDistribution(
 	nodeMap map[string]*v1.Node,
 	usageMap map[string]ResourceUsageDistributions,
@@ -536,18 +281,17 @@ func (l *LoadVariationRiskBalancing) classifyLoadDistribution(
 	return lowRiskNodes, highRiskNodes
 }
 
-func calculateRiskFromQuantities(avg, stdDev, capacity resource.Quantity) api.Percentage {
+func (l *LoadVariationRiskBalancing) calculateRiskFromQuantities(avg, stdDev, capacity resource.Quantity) api.Percentage {
 	return api.Percentage(float64(avg.MilliValue()+stdDev.MilliValue()) / float64(capacity.MilliValue()))
 }
 
-// TODO: weight between avg and vairance
-func sortNodesByUsageRisk(
+func (l *LoadVariationRiskBalancing) sortNodesByUsageRisk(
 	nodes []NodeDistributionInfo,
 	ascending bool,
 ) {
 	sort.Slice(nodes, func(i, j int) bool {
-		ti := calculateRiskFromQuantities(nodes[i].avg, nodes[i].stdDev, nodes[i].capacity)
-		tj := calculateRiskFromQuantities(nodes[j].avg, nodes[j].stdDev, nodes[j].capacity)
+		ti := l.calculateRiskFromQuantities(nodes[i].avg, nodes[i].stdDev, nodes[i].capacity)
+		tj := l.calculateRiskFromQuantities(nodes[j].avg, nodes[j].stdDev, nodes[j].capacity)
 
 		if ascending {
 			return ti < tj
@@ -557,84 +301,9 @@ func sortNodesByUsageRisk(
 	})
 }
 
-func getCPUNodeCapacities(nodes []*v1.Node) map[string]resource.Quantity {
-	capacities := map[string]resource.Quantity{}
-	for _, node := range nodes {
-		capacities[node.Name] = *node.Status.Allocatable.Cpu()
-	}
-	return capacities
-}
-
-func ResourceQuantityToPercentage(
-	value, total resource.Quantity,
-) api.Percentage {
-	return api.Percentage(value.MilliValue() / total.MilliValue())
-}
-
-func PercentageToResourceQuantity(
-	percentage api.Percentage,
-	total resource.Quantity,
-) resource.Quantity {
-	return *resource.NewMilliQuantity(
-		int64(
-			float64(total.MilliValue())*float64(percentage)*0.01,
-		), resource.BinarySI)
-}
-
-func isNodeAboveTargetRisk(nodeInfo NodeDistributionInfo) bool {
-	risk := calculateRiskFromQuantities(nodeInfo.avg, nodeInfo.stdDev, nodeInfo.capacity)
+func (l *LoadVariationRiskBalancing) isNodeAboveTargetRisk(nodeInfo NodeDistributionInfo) bool {
+	risk := l.calculateRiskFromQuantities(nodeInfo.avg, nodeInfo.stdDev, nodeInfo.capacity)
 
 	// TODO: use ita for threshold
 	return risk > 100.
-}
-
-func usageToKeysAndValues(usage resource.Quantity) []any {
-	keysAndValues := []any{}
-	keysAndValues = append(keysAndValues, "MetricResource", usage.MilliValue())
-	return keysAndValues
-}
-
-func classifyPods(pods []*v1.Pod, filter func(pod *v1.Pod) bool) ([]*v1.Pod, []*v1.Pod) {
-	var nonRemovablePods, removablePods []*v1.Pod
-
-	for _, pod := range pods {
-		if !filter(pod) {
-			nonRemovablePods = append(nonRemovablePods, pod)
-		} else {
-			removablePods = append(removablePods, pod)
-		}
-	}
-
-	return nonRemovablePods, removablePods
-}
-
-func sortPodsByRisk(
-	pods []*v1.Pod,
-	avgUsageClient, stdDevUsageClient usageClient,
-) {
-	podAvgUsage := make(map[string]*resource.Quantity)
-	podStdDevUsage := make(map[string]*resource.Quantity)
-
-	for _, pod := range pods {
-		avg, _ := avgUsageClient.podUsage(pod)
-		stdDev, _ := stdDevUsageClient.podUsage(pod)
-
-		podAvgUsage[pod.Name] = avg[MetricResource]
-		podStdDevUsage[pod.Name] = stdDev[MetricResource]
-	}
-
-	sort.Slice(pods, func(i, j int) bool {
-		pi := podAvgUsage[pods[i].Name].MilliValue() + podStdDevUsage[pods[i].Name].MilliValue()
-		pj := podAvgUsage[pods[j].Name].MilliValue() + podStdDevUsage[pods[j].Name].MilliValue()
-
-		return pi < pj
-	})
-}
-
-func quantityMapsToKeysAndValues(quantityMap map[string]resource.Quantity) []any {
-	keysAndValues := []any{}
-	for nodeName, qty := range quantityMap {
-		keysAndValues = append(keysAndValues, nodeName, qty.Value())
-	}
-	return keysAndValues
 }
---
+++ pkg/framework/plugins/nodeusagevariation/nodeusagevariation.go
@@ -0,0 +1,347 @@
+package nodeusagevariation
+
+import (
+	"context"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/descheduler/pkg/api"
+	"sigs.k8s.io/descheduler/pkg/descheduler/evictions"
+	podutil "sigs.k8s.io/descheduler/pkg/descheduler/pod"
+	"sigs.k8s.io/descheduler/pkg/framework/plugins/nodeutilization/normalizer"
+	frameworktypes "sigs.k8s.io/descheduler/pkg/framework/types"
+	"sigs.k8s.io/descheduler/pkg/utils"
+)
+
+type continueEvictionCond func(NodeDistributionInfo, resource.Quantity) bool
+
+type podSorterLowToHigh func(NodeDistributionInfo, []*v1.Pod, usageClient, usageClient)
+
+type ResourceUsageDistributions struct {
+	avg    api.Percentage
+	stdDev api.Percentage
+}
+
+type NodeDistributionUsage struct {
+	node     *v1.Node
+	avg      resource.Quantity
+	stdDev   resource.Quantity
+	capacity resource.Quantity
+	allPods  []*v1.Pod
+}
+
+// NodeInfo is an entity we use to gather information about a given node. here
+// we have its resource usage as well as the amount of available resources.
+// we use this struct to carry information around and to make it easier to
+// process.
+type NodeDistributionInfo struct {
+	NodeDistributionUsage
+	available resource.Quantity
+}
+
+func evictPodsFromSourceNodes(
+	ctx context.Context,
+	evictableNamespaces *api.Namespaces,
+	sourceNodes, destinationNodes []NodeDistributionInfo,
+	podEvictor frameworktypes.Evictor,
+	evictOptions evictions.EvictOptions,
+	podFilter func(pod *v1.Pod) bool,
+	resourceNames []v1.ResourceName,
+	continueEviction continueEvictionCond,
+	avgUsageClient, stdDevUsageClient usageClient,
+	podSorterLowToHigh podSorterLowToHigh,
+	maxNoOfPodsToEvictPerNode *uint,
+) {
+	available := assessAvailableResourceInNodes(destinationNodes)
+	klog.V(1).InfoS("Total capacity to be moved", usageToKeysAndValues(available)...)
+
+	destinationTaints := make(map[string][]v1.Taint, len(destinationNodes))
+	for _, node := range destinationNodes {
+		destinationTaints[node.node.Name] = node.node.Spec.Taints
+	}
+
+	for _, node := range sourceNodes {
+		klog.V(3).InfoS(
+			"Evicting pods from node",
+			"node", klog.KObj(node.node),
+			"avg", node.avg,
+		)
+
+		nonRemovablePods, removablePods := classifyPods(node.allPods, podFilter)
+		klog.V(2).InfoS(
+			"Pods on node",
+			"node", klog.KObj(node.node),
+			"allPods", len(node.allPods),
+			"nonRemovablePods", len(nonRemovablePods),
+			"removablePods", len(removablePods),
+		)
+
+		if len(removablePods) == 0 {
+			klog.V(1).InfoS(
+				"No removable pods on node, try next node",
+				"node", klog.KObj(node.node),
+			)
+			continue
+		}
+
+		klog.V(1).InfoS(
+			"Evicting pods based on risk, if they have same priority, they'll be evicted based on QoS tiers",
+		)
+
+		podSorterLowToHigh(node, removablePods, avgUsageClient, stdDevUsageClient)
+
+		if err := evictPods(
+			ctx,
+			evictableNamespaces,
+			removablePods,
+			node,
+			available,
+			destinationTaints,
+			podEvictor,
+			evictOptions,
+			continueEviction,
+			avgUsageClient,
+			maxNoOfPodsToEvictPerNode,
+		); err != nil {
+			switch err.(type) {
+			case *evictions.EvictionTotalLimitError:
+				return
+			default:
+			}
+		}
+	}
+}
+
+func evictPods(
+	ctx context.Context,
+	evictableNamespaces *api.Namespaces,
+	inputPods []*v1.Pod,
+	nodeInfo NodeDistributionInfo,
+	totalAvailableUsage resource.Quantity,
+	destinationTaints map[string][]v1.Taint,
+	podEvictor frameworktypes.Evictor,
+	evictOptions evictions.EvictOptions,
+	continueEviction continueEvictionCond,
+	usageClient usageClient,
+	maxNoOfPodsToEvictPerNode *uint,
+) error {
+	// preemptive check to see if we should continue evicting pods.
+	if !continueEviction(nodeInfo, totalAvailableUsage) {
+		return nil
+	}
+
+	// some namespaces can be excluded from the eviction process.
+	var excludedNamespaces sets.Set[string]
+	if evictableNamespaces != nil {
+		excludedNamespaces = sets.New(evictableNamespaces.Exclude...)
+	}
+
+	var evictionCounter uint = 0
+	for _, pod := range inputPods {
+		if maxNoOfPodsToEvictPerNode != nil && evictionCounter >= *maxNoOfPodsToEvictPerNode {
+			klog.V(3).InfoS(
+				"Max number of evictions per node per plugin reached",
+				"limit", *maxNoOfPodsToEvictPerNode,
+			)
+			break
+		}
+
+		if !utils.PodToleratesTaints(pod, destinationTaints) {
+			klog.V(3).InfoS(
+				"Skipping eviction for pod, doesn't tolerate node taint",
+				"pod", klog.KObj(pod),
+			)
+			continue
+		}
+
+		// verify if we can evict the pod based on the pod evictor
+		// filter and on the excluded namespaces.
+		preEvictionFilterWithOptions, err := podutil.
+			NewOptions().
+			WithFilter(podEvictor.PreEvictionFilter).
+			WithoutNamespaces(excludedNamespaces).
+			BuildFilterFunc()
+		if err != nil {
+			klog.ErrorS(err, "could not build preEvictionFilter with namespace exclusion")
+			continue
+		}
+
+		if !preEvictionFilterWithOptions(pod) {
+			continue
+		}
+
+		// in case podUsage does not support resource counting (e.g.
+		// provided metric does not quantify pod resource utilization).
+		unconstrainedResourceEviction := false
+		podUsageResourceList, err := usageClient.podUsage(pod)
+		if err != nil {
+			if _, ok := err.(*notSupportedError); !ok {
+				klog.Errorf(
+					"unable to get pod usage for %v/%v: %v",
+					pod.Namespace, pod.Name, err,
+				)
+				continue
+			}
+			unconstrainedResourceEviction = true
+		}
+
+		podUsage := podUsageResourceList[MetricResource]
+
+		if err := podEvictor.Evict(ctx, pod, evictOptions); err != nil {
+			switch err.(type) {
+			case *evictions.EvictionNodeLimitError, *evictions.EvictionTotalLimitError:
+				return err
+			default:
+				klog.Errorf("eviction failed: %v", err)
+				continue
+			}
+		}
+
+		if maxNoOfPodsToEvictPerNode == nil && unconstrainedResourceEviction {
+			klog.V(3).InfoS("Currently, only a single pod eviction is allowed")
+			break
+		}
+
+		evictionCounter++
+		klog.V(3).InfoS("Evicted pods", "pod", klog.KObj(pod))
+		if unconstrainedResourceEviction {
+			continue
+		}
+
+		subtractPodUsageFromNodeAvailability(&totalAvailableUsage, &nodeInfo, podUsage)
+
+		keysAndValues := []any{"node", nodeInfo.node.Name}
+		keysAndValues = append(keysAndValues, usageToKeysAndValues(nodeInfo.avg)...)
+		klog.V(3).InfoS("Updated node usage", keysAndValues...)
+
+		// make sure we should continue evicting pods.
+		if !continueEviction(nodeInfo, totalAvailableUsage) {
+			break
+		}
+	}
+	return nil
+}
+
+func subtractPodUsageFromNodeAvailability(
+	available *resource.Quantity,
+	nodeInfo *NodeDistributionInfo,
+	podUsage *resource.Quantity,
+) {
+	nodeInfo.avg.Sub(*podUsage)
+	available.Sub(*podUsage)
+}
+
+func assessAvailableResourceInNodes(
+	nodes []NodeDistributionInfo,
+) resource.Quantity {
+	available := resource.NewQuantity(0, resource.BinarySI)
+	for _, node := range nodes {
+		usage := node.avg
+
+		available.Add(node.available)
+		available.Sub(usage)
+	}
+
+	return *available
+}
+
+func rawUsageToPctUsageMap(
+	rawAvgUsage, rawStdDevUsage, rawCapacities map[string]resource.Quantity,
+) map[string]ResourceUsageDistributions {
+	avgUsage := normalizer.Normalize(
+		rawAvgUsage, rawCapacities, ResourceQuantityToPercentage,
+	)
+
+	stdDevUsage := normalizer.Normalize(
+		rawStdDevUsage, rawCapacities, ResourceQuantityToPercentage,
+	)
+
+	usageMap := make(map[string]ResourceUsageDistributions)
+	for nodeName := range rawAvgUsage {
+		usageMap[nodeName] = ResourceUsageDistributions{
+			avg:    avgUsage[nodeName],
+			stdDev: stdDevUsage[nodeName],
+		}
+	}
+
+	return usageMap
+}
+
+func getNodeUsageDistributionSnapshot(
+	nodes []*v1.Node,
+	avgUsageClient usageClient,
+	stdDevUsageClient usageClient,
+) (
+	map[string]*v1.Node,
+	map[string]resource.Quantity,
+	map[string]resource.Quantity,
+	map[string][]*v1.Pod,
+) {
+	rawAvgUsage := make(map[string]resource.Quantity)
+	rawStdDevUsage := make(map[string]resource.Quantity)
+	podListMap := make(map[string][]*v1.Pod)
+	nodesMap := make(map[string]*v1.Node)
+
+	for _, node := range nodes {
+		nodesMap[node.Name] = node
+		rawAvgUsage[node.Name] = *avgUsageClient.nodeUtilization(node.Name)[MetricResource]
+		rawStdDevUsage[node.Name] = *stdDevUsageClient.nodeUtilization(node.Name)[MetricResource]
+		podListMap[node.Name] = avgUsageClient.pods(node.Name)
+	}
+
+	return nodesMap, rawAvgUsage, rawStdDevUsage, podListMap
+}
+
+func getCPUNodeCapacities(nodes []*v1.Node) map[string]resource.Quantity {
+	capacities := map[string]resource.Quantity{}
+	for _, node := range nodes {
+		capacities[node.Name] = *node.Status.Allocatable.Cpu()
+	}
+	return capacities
+}
+
+func ResourceQuantityToPercentage(
+	value, total resource.Quantity,
+) api.Percentage {
+	return api.Percentage(value.MilliValue() / total.MilliValue())
+}
+
+func PercentageToResourceQuantity(
+	percentage api.Percentage,
+	total resource.Quantity,
+) resource.Quantity {
+	return *resource.NewMilliQuantity(
+		int64(
+			float64(total.MilliValue())*float64(percentage)*0.01,
+		), resource.BinarySI)
+}
+
+func usageToKeysAndValues(usage resource.Quantity) []any {
+	keysAndValues := []any{}
+	keysAndValues = append(keysAndValues, "MetricResource", usage.MilliValue())
+	return keysAndValues
+}
+
+func classifyPods(pods []*v1.Pod, filter func(pod *v1.Pod) bool) ([]*v1.Pod, []*v1.Pod) {
+	var nonRemovablePods, removablePods []*v1.Pod
+
+	for _, pod := range pods {
+		if !filter(pod) {
+			nonRemovablePods = append(nonRemovablePods, pod)
+		} else {
+			removablePods = append(removablePods, pod)
+		}
+	}
+
+	return nonRemovablePods, removablePods
+}
+
+func quantityMapsToKeysAndValues(quantityMap map[string]resource.Quantity) []any {
+	keysAndValues := []any{}
+	for nodeName, qty := range quantityMap {
+		keysAndValues = append(keysAndValues, nodeName, qty.Value())
+	}
+	return keysAndValues
+}
